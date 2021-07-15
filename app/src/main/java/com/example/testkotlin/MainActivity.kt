67e52c72f99b10999e097ecff8945b1bcc51267f
Hatidzhe Onbashieva <hatidzhe.onbashieva@flatrocktech.com>
2021-07-15 14:03:36 +0300

    second commit

--- app/src/main/java/com/example/testkotlin/MainActivity.kt
+++ app/src/main/java/com/example/testkotlin/MainActivity.kt
@@ -3,8 +3,10 @@ package com.example.testkotlin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-val topLevelName = "Hatidzhe"; //top-level variables; moved to the .kt file; can be used as local variables
-var topLevelNullname:String? = null; //making a variable nullable; means that a variable can be assigned a null value
+val topLevelName =
+    "Hatidzhe"; //top-level variables; moved to the .kt file; can be used as local variables
+var topLevelNullname: String? =
+    null; //making a variable nullable; means that a variable can be assigned a null value
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,67 +18,15 @@ class MainActivity : AppCompatActivity() {
         val nickname: String =
             "Hati" //local read-only variables which can be assigned only once; if i try to update name - not possible
 
+        println("The full name value: $fullName")
+        println("The nickname value: $nickname")
 
+        println("Top-level name is $topLevelName")
 
-        val newTopLevelName1: Int? = topLevelNullname?.length;
-        topLevelNullname = "Null name var"
-        val newTopLevelName2: Int = topLevelNullname!!.length;
-        println(newTopLevelName1)
-        println(newTopLevelName2)
-
-        val nullableList: List<Int?> = listOf(1, 2, null, 4)
-        val intList: List<Int> = nullableList.filterNotNull()
-        println(intList)
-
-        var a = 1
-// simple name in template:
-        val s1 = "a is $a"
-
-        a = 2
-// arbitrary expression in template:
-        val s2 = "${s1.replace("is", "was")}, but now is $a"
-        println(s2)
-
-        val listOfItems = listOf("banana", "kiwi", "apple")
-        for(index in listOfItems.indices)
-            println("fruit at index $index is ${listOfItems[index]}")
-
-        var index = 0
-        while(index < listOfItems.size)
-        {
-            println("fruit at index $index is ${listOfItems[index]}")
-            index++
-        }
-
-        val x:Any = "Hello";
-        var variable: String? = when (x){
-             1 -> "x is one"
-            2 -> "x is two"
-            3 -> "x is three"
-            else -> "unknown value"
-        }
-
-        println(variable)
-
-        val y = 4
-        if (y in 10.downTo(1) step 2) println(y) else println("not in range") // it is going to search for 4 starting from 10 and going down to 0 with step 2 - 10, 8, 6, 4,2,0
-
-        val fruits = listOf("banana", "avocado", "apple", "kiwifruit")
-        fruits
-            .filter { it.startsWith('k') }
-            .sortedBy { it }
-            .map { it.uppercase() }
-            .forEach{(println(it))}
-        //println(fullName)
-        //println(nickname)
-
-        //println(topLevelName)
-
-        /* when(fullName)
-        {
-            !is String -> {
+        when (fullName) {
+            "Hatidzhe Onbashieva" -> {
                 fullName = "Hati"
-             println(fullName)
+                println(fullName)
             }
 
             else -> println("When loop")
@@ -86,31 +36,107 @@ class MainActivity : AppCompatActivity() {
         println(getGreeting())
         hello()
 
-    }
+        val newTopLevelName1: Int? = topLevelNullname?.length;
+        println("The length of topLevelName(with the default value) is $newTopLevelName1")
 
-    fun getGreeting(): String{
+        topLevelNullname = "Some name here"
+        val newTopLevelName2: Int =
+            topLevelNullname!!.length; //used only when we know that a nullable variable is assigned a value and making a new non-nullable variable; if the value is null -> throws a NullPointerException
+        println("The length of topLevelName(with a value assigned) is $newTopLevelName2")
 
-        return "Hello Kotlin"
-    }
+        //searching a list for nullable objects and removing them from the list
+        val nullableList: List<Int?> = listOf(1, 2, null, 4)
+        val intList: List<Int> = nullableList.filterNotNull()
+        println(intList)
 
-    fun hello()
-    {
-        println(getGreeting())
-    }*/
+        var a = 1
+        val s1 = "a is $a" // simple name in template:
+
+        a = 2
+        val s2 = "${s1.replace("is", "was")}, but now is $a" // arbitrary expression in template
+        println("s2 values is $s2")
+
+        //for loop to search in a list
+        val listOfItems = listOf("banana", "kiwi", "apple")
+        for (index in listOfItems.indices)
+            println("fruit at index $index is ${listOfItems[index]}")
+
+        //while loop to search in a list
+        var index = 0
+        while (index < listOfItems.size) {
+            println("fruit at index $index is ${listOfItems[index]}")
+            index++
+        }
+
+        //when expression -> similar to switch in Java
+        val x: Any = "Hello";
+        var variable: String? = when (x) {
+            1 -> "x is one"
+            2 -> "x is two"
+            3 -> "x is three"
+            else -> "unknown value"
+        }
+        println("When loop variable: $variable")
+
+        val y = 4
+        if (y in 10.downTo(1) step 2) println(y) else println("not in range") // it is going to search for 4 starting from 10 and going down to 0 with step 2 - 10, 8, 6, 4,2,0
+
+        val fruits = listOf("banana", "avocado", "apple", "kiwifruit")
+        fruits
+            .filter { it.startsWith('k') }
+            .sortedBy { it }
+            .map { it.uppercase() }
+            .forEach { (println(it)) }
+
+        for (fruit in fruits.sorted()) {
+            if (fruit.startsWith('a')) {
+                println(fruit.uppercase())
+            }
+        }
 
         val person = Person("Ivan", "Petrov")
         //person.firstname is known as property access syntax; in Kotlin we can directly reference properties by their name without worrying about a getter or setter
-        println("\nFirst name: ${person.firstname} \nLast name: ${person.lastName}")
+        println("\nFirst name of my Person: ${person.firstname} \nLast name of my Person: ${person.lastName}")
 
         val map = mapOf("a" to 1, "b" to 2, "c" to 3)
         println(map["a"])
         println(map.getValue("a"))
         //map.forEach { key, value -> println("$key = $value")}
-        for((key, value) in map)
-            println("$key = $value")
+        for ((key, value) in map)
+            println("$key = $value ")
 
-        infix fun String.onto(other: String) = Pair(this, other)
-        val myPair = "McLaren" onto "Lucas"
-        println(myPair)
+        infix fun String.onto(other: String) =
+            Pair(this, other) // infix functions can be used for only single parameter
+        println("McLaren" onto "Lucas")
+
+        infix fun Int.times(value: String) = value.repeat(this)
+        println(3 times "Hello ")
+
+        infix fun String.findIn(value: String) = value.contains(this) //returns a Boolean value
+        println("heyy" findIn "Hello world!")
+
+        fun printAll(vararg messages: String) {                            // 1
+            for (m in messages) println(m)
+        }
+
+        printAll("Hello", "Hallo", "Salut", "Hola", "你好")
+
+        fun log(vararg entries: String) {
+            printAll(*entries)                                             // 5
+        }
+
     }
+
+
+    //fun String getGreet(value: String) = "Hello there"
+
+    fun getGreeting(): String {
+
+        return "Hello Kotlin"
+    }
+
+    fun hello() {
+        println(getGreeting())
+    }
+
 }
